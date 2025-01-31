1b352cc2a647da7c651b353243fc01600c7a83ad
Ne'matov Saidakbar <Saidakbarnematov2509@gamil.com>
2025-01-31 12:01:12 +0500

    Signals

--- products/signals.py
+++ products/signals.py
@@ -1,9 +1,8 @@
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from django.db.models.signals import post_save
-from django.core.exceptions import ValidationError
 
-from products.models import ProductModel,ProductImageModel
+
+from products.models import ProductModel
 
 
 @receiver(pre_save, sender=ProductModel)
@@ -12,10 +11,3 @@ def calculate_discount_price(sender, instance, **kwargs):
         instance.discount_price = instance.price - (instance.price * instance.discount / 100)
     else:
         instance.discount_price = instance.price
-
-@receiver(post_save, sender=ProductImageModel)
-def check_minimum_images(sender, instance, **kwargs):
-    image_count = instance.image.count()
-
-    if image_count < 4:
-        raise ValidationError("Har bir mahsulot kamida 4 ta rasmga ega bo‘lishi kerak!")
